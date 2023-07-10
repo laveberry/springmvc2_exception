d5d6f64b0d55ed0af5ef1d62d2b9e139dc758db6
laveberry <laveberry7@gmail.com>
2023-07-11 06:10:13 +0900

    서블릿 예외처리 - 시작, 오류화면제공

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+//과거에는 web.xml 에서 에러페이지 처리
+//Component 등록
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        //오류나면 was까지 올라간후 was에서 해당 에러페이지 호출
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        //RuntimeException의 자식들도 해당 페이지로 이동됨
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        //에러페이지 등록
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
